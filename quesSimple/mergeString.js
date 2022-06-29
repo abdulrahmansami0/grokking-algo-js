d10d08125544f60613d6e248ffc39d310b5441bf
Abdalrhman Sami <abdalrhmansami@Abdalrhmans-MacBook-Pro.local>
2022-06-29 16:28:48 +0300

    simple ques 4

---
+++ quesSimple/mergeString.js
@@ -0,0 +1,20 @@
+var mergeAlternately = function (word1, word2) {
+  let count1 = 0;
+  let count2 = 0;
+  let newStr = [];
+  let newWord1 = word1.split("");
+  let newWord2 = word2.split("");
+  for (let i = 0; i < newWord1.length + newWord2.length; ++i) {
+    if (count1 !== newWord1.length) {
+      newStr.push(newWord1[i]);
+      count1++;
+    }
+    if (count2 !== newWord2.length) {
+      newStr.push(newWord2[i]);
+      count2++;
+    }
+  }
+  return newStr.join("");
+};
+
+mergeAlternately("abd", "cdf");
--- quesSimple/symmetricTree.js
+++ quesSimple/symmetricTree.js
@@ -8,33 +8,63 @@ class Node {
 
 const A = new Node("A");
 const B = new Node("B");
-const C = new Node("B");
-const D = new Node("C");
-const E = new Node("C");
+const C = new Node("C");
+const D = new Node("D");
+const E = new Node("E");
+const F = new Node("F");
 
 A.left = B;
 A.right = C;
 B.left = D;
 B.right = E;
-C.left = D;
-C.right = E;
+C.right = F;
 
-const are_symmetric = (root1, root2) => {
-  if (!root1 && !root2) return true;
-  else if (!root1 !== !root2 || root1.value !== root2.value) return false;
-  else {
-    return (
-      are_symmetric(root1.left, root2.right) &&
-      are_symmetric(root1.right, root2.left)
-    );
-  }
-};
+// const are_symmetric = (root1, root2) => {
+//   if (!root1 && !root2) return true;
+//   else if (!root1 !== !root2 || root1.value !== root2.value) return false;
+//   else {
+//     return (
+//       are_symmetric(root1.left, root2.right) &&
+//       are_symmetric(root1.right, root2.left)
+//     );
+//   }
+// };
 
+// const is_symmetric = (root) => {
+//   if (!root) {
+//     return true;
+//   }
+//   return are_symmetric(root.left, root.right);
+// };
+
+// not sure
 const is_symmetric = (root) => {
-  if (!root) {
-    return true;
+  if (!root) return true;
+  while (root.left && root.right) {
+    if (!root.left && !root.right) return true;
+    if (!root.left !== !root.right || root.left.value !== root.right.value)
+      return false;
+    root = root.left;
+    root = root.right;
   }
-  return are_symmetric(root.left, root.right);
+  return root.left === root.right;
 };
-
 console.log(is_symmetric(A));
+
+const getAll = (root) => {
+  const result = [];
+  const stack = [root];
+  while (stack.length) {
+    const current = stack.pop();
+
+    result.push(current.value);
+    if (current.left) {
+      stack.push(current.left);
+    }
+    if (current.right) {
+      stack.push(current.right);
+    }
+  }
+  return result;
+};
+console.log(getAll(A));
