44b8182bfb054bff6a031946c9d7c0783a6601e4
abdalrhmansami <abdalrhmansami5@gmail.com>
2022-09-14 15:39:49 +0300

    add buildArray

---
+++ array-problems/buildArrayfromPermutation.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+const buildArray = function (nums) {
+  let ans = [];
+  for (let i = 0; i < nums.length; ++i) {
+    ans[i] = nums[nums[i]];
+  }
+  return ans;
+};
+
+let nums = [0, 2, 1, 5, 3, 4];
+console.log(buildArray(nums));
