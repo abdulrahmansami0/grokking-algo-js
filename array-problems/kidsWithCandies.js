c9a5ab9f132f17db78003a2a2a3004beb04df274
abdalrhmansami <abdalrhmansami5@gmail.com>
2022-09-14 22:50:19 +0300

    add kidsWithCandies

---
+++ array-problems/kidsWithCandies.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+const kidsWithCandies = (candies, extraCandies) => {
+  let result = [];
+  for (let i = 0; i < candies.length; ++i) {
+    let summation = candies[i] + extraCandies;
+    result.push(false);
+    for (let j = 0; j < candies.length; ++j) {
+      if (summation < candies[j]) {
+        result[i] = false;
+        break;
+      } else {
+        result[i] = true;
+      }
+    }
+  }
+  return result;
+};
+
+let candies = [2, 3, 5, 1, 3];
+let extraCandies = 3;
+kidsWithCandies(candies, extraCandies);
