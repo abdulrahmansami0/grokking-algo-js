ecc006d1ae1b6aca2b86b15eb6b5ef59f99b37e8
abdalrhmansami <abdalrhmansami5@gmail.com>
2022-09-14 15:36:56 +0300

    add count matches

---
+++ array-problems/countMatches.js
@@ -0,0 +1,18 @@
+const countMatches = (items, ruleKey, ruleValue) => {
+  let keyRule = ["type", "color", "name"];
+  let count = 0;
+  for (let i = 0; i < items.length; ++i) {
+    for (let j = 0; j < items[i].length; ++j) {
+      if (items[i][j] === ruleValue && keyRule[j] === ruleKey) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
+
+let items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]];
+let ruleKey = "color";
+let ruleValue = "silver";
+
+countMatches(items, ruleKey, ruleValue)
