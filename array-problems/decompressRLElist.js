79cdb86f73839d065614f900540baa0e953fddb1
abdalrhmansami <abdalrhmansami5@gmail.com>
2022-09-15 17:01:43 +0300

    add decompressRLElis2 O(n) only

--- array-problems/decompressRLElist.js
+++ array-problems/decompressRLElist.js
@@ -3,6 +3,7 @@
  * @return {number[]}
  */
 
+// Way with O(n^2)
 const decompressRLElist = (nums) => {
   let result = [];
   for (let i = 0; i < nums.length; ++i) {
@@ -16,4 +17,16 @@ const decompressRLElist = (nums) => {
   return result;
 };
 
+console.log(decompressRLElist([1, 2, 3, 4]));
+
+// Use the second way O(n)
+const decompressRLElist2 = (nums) => {
+  const result = [];
+
+  for (let i = 1; i < n; i += 2) {
+    result.push(...new Array(nums[i - 1]).fill(nums[i]));
+  }
+
+  return result;
+};
 console.log(decompressRLElist([1, 2, 3, 4]));
