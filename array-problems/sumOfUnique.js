acde2edabcfb4e3802dea9c2f020b7fe952d87d0
12580390 <abdalrhmansami5@gmail.com>
2022-09-21 01:52:28 +0300

    add sumOfUnique

---
+++ array-problems/sumOfUnique.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const sumOfUnique = (nums) => {
+  let hashMap = {};
+  let sum = 0;
+  for (let i = 0; i < nums.length; ++i) {
+    if (!hashMap[nums[i]]) {
+      hashMap[nums[i]] = 1;
+    } else {
+      hashMap[nums[i]]++;
+    }
+  }
+
+  for (const [key, value] of Object.entries(hashMap)) {
+    if (value === 1) {
+      sum += +key;
+    }
+  }
+  return sum;
+};
+
+console.log(sumOfUnique([1, 2, 3, 2]));
