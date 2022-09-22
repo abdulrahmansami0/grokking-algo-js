c6247573242e7a4f393cac0ec6771a85e06ff659
12580390 <abdalrhmansami5@gmail.com>
2022-09-22 12:40:00 +0300

    add binary search

---
+++ array-problems/binarySearch.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const search = (nums, target) => {
+  if (!nums.length) return -1;
+  let left = 0;
+  let right = nums.length - 1;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    console.log(mid);
+    if (nums[mid] === target) return mid;
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    }
+  }
+  return -1;
+};
+
+console.log(search([-1, 0, 3, 5, 9, 12], 9));
