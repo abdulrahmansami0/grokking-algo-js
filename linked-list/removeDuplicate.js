7e30595c1326276ab7a2242a192d70f3ed1dec6f
12580390 <abdalrhmansami5@gmail.com>
2022-09-25 19:05:41 +0300

    add removeDuplicate

---
+++ linked-list/removeDuplicate.js
@@ -0,0 +1,42 @@
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+const a = new Node(100);
+const b = new Node(200);
+const c = new Node(300);
+const d = new Node(400);
+const e = new Node(300);
+const f = new Node(600);
+const g = new Node(700);
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+e.next = f;
+f.next = g;
+
+const removeDuplicateElement = (head) => {
+  let outerNode = head;
+  while (outerNode != null) {
+    let innerNode = outerNode; // Iterator for the inner loop
+    while (innerNode != null) {
+      if (
+        innerNode.nextElement != null &&
+        outerNode.data == innerNode.nextElement.data
+      ) {
+        //Duplicate found ahead
+        innerNode.nextElement = innerNode.nextElement.nextElement; // Remove duplicate
+      } else {
+        innerNode = innerNode.nextElement; // Otherwise simply iterate ahead
+      }
+    }
+    outerNode = outerNode.nextElement;
+  }
+
+  return head;
+};
