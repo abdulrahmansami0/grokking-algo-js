85a36ac3ed3c610cd498e6cebd47d9072247d52d
12580390 <abdalrhmansami5@gmail.com>
2022-09-27 22:24:14 +0300

    add insertion sort

---
+++ sorting/insertionSort.js
@@ -0,0 +1,14 @@
+const insertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let j = i - 1;
+    let key = arr[i];
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
+  }
+  return arr;
+};
+
+console.log(insertionSort([2, 4, 1, 6, 8, 2, 6]));
