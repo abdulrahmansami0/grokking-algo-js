7ffebd74fc57f3441e621dc664f4012fab923d3d
12580390 <abdalrhmansami5@gmail.com>
2023-06-23 15:27:48 +0300

    fix three sum

--- array-problems/threeSum.js
+++ array-problems/threeSum.js
@@ -22,37 +22,37 @@ const threeSum = (arr, targetSum) => {
 
 console.log(threeSum([12, 3, 1, 2, -6, 5, -8, 6], 0));
 
-/**
- *
- * @param {number[]} leetcode
- * @return {number[][]}
- */
+// /**
+//  *
+//  * @param {number[]} leetcode
+//  * @return {number[][]}
+//  */
 
-const threeSum2 = (nums) => {
-  nums.sort((a, b) => a - b);
-  const n = nums.length;
-  const triplets = [];
-  for (let i = 0; i < n; i++) {
-    if (i > 0 && nums[i] === nums[i - 1]) {
-      continue;
-    }
-    let left = i + 1;
-    let right = n - 1;
-    while (left < right) {
-      if (nums[i] + nums[left] + nums[right] === 0) {
-        triplets.push([nums[i], nums[left], nums[right]]);
-        left++;
-        while (left < right && nums[left] === nums[left - 1]) {
-          left++;
-        }
-      } else if (nums[i] + nums[left] + nums[right] < 0) {
-        left++;
-      } else {
-        right--;
-      }
-    }
-  }
-  return triplets;
-};
+// const threeSum2 = (nums) => {
+//   nums.sort((a, b) => a - b);
+//   const n = nums.length;
+//   const triplets = [];
+//   for (let i = 0; i < n; i++) {
+//     if (i > 0 && nums[i] === nums[i - 1]) {
+//       continue;
+//     }
+//     let left = i + 1;
+//     let right = n - 1;
+//     while (left < right) {
+//       if (nums[i] + nums[left] + nums[right] === 0) {
+//         triplets.push([nums[i], nums[left], nums[right]]);
+//         left++;
+//         while (left < right && nums[left] === nums[left - 1]) {
+//           left++;
+//         }
+//       } else if (nums[i] + nums[left] + nums[right] < 0) {
+//         left++;
+//       } else {
+//         right--;
+//       }
+//     }
+//   }
+//   return triplets;
+// };
 
-console.log(threeSum2([12, 3, 1, 2, -6, 5, -8, 6]));
+// console.log(threeSum2([12, 3, 1, 2, -6, 5, -8, 6]));
