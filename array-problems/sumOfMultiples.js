490e2c63d65ede8b76357c5ebcd2ee290ecfa6c5
12580390 <abdalrhmansami5@gmail.com>
2023-06-24 15:28:58 +0300

    add sumOfMultiples

---
+++ array-problems/sumOfMultiples.js
@@ -0,0 +1,19 @@
+// Input: n = 7
+// Output: 21
+// Explanation: Numbers in the range [1, 7] that are divisible by 3, 5, or 7 are 3, 5, 6, 7. The sum of these numbers is 21.
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const sumOfMultiples = (n) => {
+  let number = 0;
+  for (let i = 1; i <= n; ++i) {
+    if (i % 3 === 0 || i % 5 === 0 || i % 7 === 0) {
+      number += i;
+    }
+  }
+
+  return number;
+};
+
+console.log(sumOfMultiples(7));
