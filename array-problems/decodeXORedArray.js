745edbc7e50a7991644c4c930275a0691cf757ce
12580390 <abdalrhmansami5@gmail.com>
2023-06-24 14:58:10 +0300

    add decode XORed Array

---
+++ array-problems/decodeXORedArray.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} encoded
+ * @param {number} first
+ * @return {number[]}
+ */
+const decode = (encoded, first) => {
+  let arr = [];
+  arr[0] = first;
+
+  for (let i = 0; i < encoded.length; ++i) {
+    arr[i + 1] = encoded[i] ^ arr[i];
+  }
+
+  return arr;
+};
