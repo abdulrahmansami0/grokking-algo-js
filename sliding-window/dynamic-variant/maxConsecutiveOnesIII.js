362969088d3db60df91340d270c18008f8bc91b8
12580390 <abdalrhmansami5@gmail.com>
2024-03-22 17:21:54 +0300

    solve Max Consecutive Ones III

---
+++ sliding-window/dynamic-variant/maxConsecutiveOnesIII.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var longestOnes = function (nums, k) {
+  let left = 0;
+  let result = 0;
+  let zeroCount = 0;
+
+  for (let right = 0; right < nums.length; ++right) {
+    if (nums[right] === 0) {
+      zeroCount++;
+    }
+
+    while (zeroCount > k) {
+      if (nums[left] === 0) {
+        zeroCount--;
+      }
+      left++;
+    }
+
+    result = Math.max(result, right - left + 1);
+  }
+
+  return result;
+};
+
+// Example 1:
+
+// Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+// Output: 6
+// Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+// Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+// Example 2:
+
+// Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+// Output: 10
+// Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+// Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
