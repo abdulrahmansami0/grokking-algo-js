4130351cd2688f61cc65ceb342c8e59cf01acf6e
12580390 <abdalrhmansami5@gmail.com>
2024-04-02 14:31:16 +0300

    add freqAlphabets

---
+++ array-problems/freqAlphabets.js
@@ -0,0 +1,59 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var freqAlphabets = function (s) {
+  let stringSplitter = "";
+  let result = "";
+  let hash = {};
+
+  for (let i = 1; i <= 26; ++i) {
+    hash[i] = String(97 + i - 1);
+  }
+
+  for (let i = 0; i < s.length; ++i) {
+    if (s[i + 2] === "#") {
+      stringSplitter += s[i] + s[i + 1];
+      stringSplitter += "#";
+    } else if (s[i + 1] === "#") {
+      continue;
+    } else {
+      stringSplitter += s[i];
+      stringSplitter += "#";
+    }
+  }
+
+  stringSplitter.split("#").forEach((el) => {
+    if (hash[el]) {
+      result += String.fromCharCode(hash[el]);
+    }
+  });
+
+  return result;
+};
+
+// You are given a string s formed by digits and '#'. We want to map s to English lowercase characters as follows:
+
+//     Characters ('a' to 'i') are represented by ('1' to '9') respectively.
+//     Characters ('j' to 'z') are represented by ('10#' to '26#') respectively.
+
+// Return the string formed after mapping.
+
+// The test cases are generated so that a unique mapping will always exist.
+
+// Example 1:
+
+// Input: s = "10#11#12"
+// Output: "jkab"
+// Explanation: "j" -> "10#" , "k" -> "11#" , "a" -> "1" , "b" -> "2".
+
+// Example 2:
+
+// Input: s = "1326#"
+// Output: "acz"
+
+// Constraints:
+
+//     1 <= s.length <= 1000
+//     s consists of digits and the '#' letter.
+//     s will be a valid string such that mapping is always possible.
